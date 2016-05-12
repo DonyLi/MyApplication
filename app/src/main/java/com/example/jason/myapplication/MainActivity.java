0fc12cfb23cfc3b12de6c33a1b8c3501d590d801
Jason <348543036@qq.com>
2016-05-12 11:56:39 +0800

    修改了一次

--- app/src/main/java/com/example/jason/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jason/myapplication/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.jason.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.e("ss", "修改测试");
     }
 }
